54e2557de225d41ea8685cf95d4a096f2d851738
nghia7217 <nghia7217@gmail.com>
2021-01-09 22:54:20 +0700

    hu

---
+++ ShopEcommerce/Models/BannerCategory.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ShopEcommerce.Models {
+    public class BannerCategory {
+        [Key]
+        public Guid Id { get; set; }
+        [Display(Name = "Tên")]
+        public string Name { get; set; }
+        [Display(Name = "Mô tả")]
+        public string Description { get; set; }
+        [Display(Name = "STT")]
+        public int OrderNo { get; set; }
+
+        [Display(Name = "Được xuất bản")]
+        public bool? IsPublish { get; set; }
+
+        [ForeignKey("UserCreated")]
+        [Display(Name = "Người tạo")]
+       
+        public DateTime CreatedDate { get; set; }
+        public ApplicationUser UserCreated { get; set; }
+
+        [ForeignKey("UserModified")]
+        [Display(Name = "Người cập nhật")]
+     
+        public DateTime? ModifiedDate { get; set; }
+        public ApplicationUser UserModified { get; set; }
+
+        public virtual IList<Banner> Banners { get; set; }
+
+        public BannerCategory() {
+            Banners = new List<Banner>();
+        }
+    }
+}
---
+++ ShopEcommerce/Models/ConfigSetting.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ShopEcommerce.Models {
+    public class ConfigSetting {
+
+        [Key]
+        public Guid Id { get; set; }
+
+        [Display(Name = "Tên viết tắt")]
+        public string AppNameShort { get; set; }
+
+      
+
+        [Display(Name = "Logo công ty")]
+        public string AppLogo { get; set; }
+
+        [Display(Name = "Tiêu đề trang web")]
+        public string Title { get; set; }
+
+        [Display(Name = "Số điện thoại")]
+        public string PhoneNumber { get; set; }
+        [Display(Name = "Di động")]
+        public string MobilePhone { get; set; }
+        [Display(Name = "Địa chỉ email")]
+        public string Email { get; set; }
+        [Display(Name = "Địa chỉ liên hệ")]
+        public string Address { get; set; }
+
+
+        [Display(Name = "Chính sách bán hàng")]
+        public string Policy { get; set; }
+        [Display(Name = "Dịch vụ")]
+        public string Service { get; set; }
+    }
+}
